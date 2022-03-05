3fcf77de10df2e4ea56936a2a99168e58c3010d7
NPraglin <npraglin@gmail.com>
2022-03-05 15:01:16 -0600

    Updated registration forms

--- users/forms.py
+++ users/forms.py
@@ -7,7 +7,7 @@ from .models import Profile
 class UserRegisterForm(UserCreationForm):
   email = forms.EmailField()
   island_boi = forms.CharField(
-        label="Are you an island boi?",
+        label="Do you know how to play chess?",
         max_length=90,
         widget=forms.TextInput(),
         required=True,
--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
     if form.is_valid():
       form.save()
       username = form.cleaned_data.get('username')
-      messages.success(request, f'Account created for {username}! You may now login, islandboi.')
+      messages.success(request, f'Account created for {username}! You may now login.')
       return redirect('login')
   else: 
     form = UserRegisterForm()
